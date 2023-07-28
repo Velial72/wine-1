5e89bdf12e36aea2e42a644d99b2554e68967fbd
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-07-28 20:05:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,28 +8,27 @@ from collections import defaultdict
 
 import found_age
 
-
-def get_wine_catalog():
-    exel_data_df = pd.read_excel('wine.xlsx')
-    types_of_wine = exel_data_df.to_dict(orient="records")
+def get_wine_catalog(wine_cards_filepath):
+    excel_data_df = pd.read_excel(wine_cards_filepath, sheet_name='Лист1', na_values='nan', keep_default_na=False)
+    wines = excel_data_df.to_dict(orient='records')
     grouped_wine_cards = defaultdict(list)
-    for wine in types_of_wine:
+    for wine in wines:
         grouped_wine_cards[wine['Категория']].append(wine)
     return grouped_wine_cards
 
+
 def get_dir_path():
     parser = argparse.ArgumentParser(description='Запуск Сайта-магазина')
-    parser.add_argument('-d', '--dir', help='Укажите путь к файлу с продукцией, по умолчанию wine_catalog.xlsx',
-                        default='wine_catalog.xlsx')
+    parser.add_argument('-d', '--dir', help='Укажите путь к файлу с продукцией, по умолчанию wine.xlsx',
+                        default='wine.xlsx')
     args = parser.parse_args()
     return args.dir
     
 
 if __name__ == '__main__':
-
-    year_of_opening = 1920
-    winery_age = found_age.get_age_winery(),
-    correct_year_name = found_age.get_age_winery()
+    current_date = datetime.datetime.now()
+    winery_age = str(current_date.year - 1920)
+    correct_winery_age = found_age.get_winery_age(winery_age),
     wines_catalog = get_wine_catalog(get_dir_path())
     env = Environment(
         loader=FileSystemLoader('.'),
@@ -38,8 +37,7 @@ if __name__ == '__main__':
     template = env.get_template('template.html')
     rendered_page = template.render(
         wines_catalog=wines_catalog,
-        winery_age=winery_age,
-        correct_year_name=correct_year_name,
+        winery_age=correct_winery_age,
     )
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
