c175a313bf214d8284f5937b3a635b82b9a653ae
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-07-28 20:04:44 +0500

    Update found_age.py

--- found_age.py
+++ found_age.py
@@ -1,11 +1,4 @@
-import datetime
-
-
-current_date = datetime.datetime.now()
-winery_age = str(current_date.year - 1920)
-
-
-def get_winery_age():
+def get_winery_age(winery_age):
     if winery_age[-1] == '1':
         age = f'{winery_age} год'
     elif winery_age[-1] in '234':
