31e4985743c8ffcdb3d7958661f10a3d3dd79536
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-07-30 20:15:17 +0500

    Delete found_age.py
    убрал лишнее

--- found_age.py
+++
@@ -1,8 +0,0 @@
-def get_winery_age(winery_age):
-    if winery_age[-1] == '1':
-        age = f'{winery_age} год'
-    elif winery_age[-1] in '234':
-        age = f'{winery_age} года'
-    else:
-        age = f'{winery_age} лет'
-    return age
